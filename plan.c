ab9d155690aae29114c1ae9dd31373bdd8424aea
amontalb <amontalb@student.42lyon.fr>
2023-01-24 11:14:50 +0100

    .

--- plan.c
+++ plan.c
@@ -2,4 +2,11 @@ Récupérer en boucle l’entrée de l’utilisateur
 Parser l’entrée utilisateur
 Executer la commande
 Coder les builtins
+	◦ echo et l’option -n
+◦ cd uniquement avec un chemin relatif ou absolu
+◦ pwd sans aucune option
+◦ export sans aucune option
+◦ unset sans aucune option
+◦ env sans aucune option ni argument
+◦ exit sans aucune option
 Gestion de l’environnement
