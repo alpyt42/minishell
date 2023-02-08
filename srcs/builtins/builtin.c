3e21a3fd6e631c6fda9998a64398e4455a9f3bd4
AlricPerso <alric.conte@gmail.com>
2023-02-08 19:31:54 +0100

    up

--- srcs/builtins/builtin.c
+++ srcs/builtins/builtin.c
@@ -6,8 +6,15 @@
 /*   By: ale-cont <ale-cont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 17:40:09 by ale-cont          #+#    #+#             */
-/*   Updated: 2023/01/30 19:38:34 by ale-cont         ###   ########.fr       */
+/*   Updated: 2023/02/08 18:44:35 by ale-cont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+extern int	s_error;
+
+int	check_builtin(t_data *data)
+{
+	
+}
