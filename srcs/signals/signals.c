ec7346f598260bc81f0fdb00b673cd8f526b465f
AlricPerso <alric.conte@gmail.com>
2023-02-13 19:55:09 +0100

    up

--- srcs/signals/signals.c
+++ srcs/signals/signals.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amontalb <amontalb@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: ale-cont <ale-cont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 12:12:05 by ale-cont          #+#    #+#             */
-/*   Updated: 2023/02/10 10:06:51 by amontalb         ###   ########.fr       */
+/*   Updated: 2023/02/13 16:56:12 by ale-cont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 int	s_error;
 
-// void	signal_receive(int sig)
-// {
-// 	if (sig == SIGINT)
-// 	{
-// 		s_error = 130;
-// 		ioctl(STDIN_FILENO, TIOCSTI, "\n");
-// 		rl_replace_line("", 0);
-// 		rl_on_new_line();
-// 	}
-// }
+void	signal_receive(int sig)
+{
+	if (sig == SIGINT)
+	{
+		s_error = 130;
+		ioctl(STDIN_FILENO, TIOCSTI, "\n");
+		rl_replace_line("", 0);
+		rl_on_new_line();
+	}
+}
