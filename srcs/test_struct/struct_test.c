272f34d51abe4be0df7340c5b56c570d56879b7d
Alric42Mac <ale-cont@student.42lyon.fr>
2023-02-28 10:18:46 +0100

    up

--- srcs/test_struct/struct_test.c
+++ srcs/test_struct/struct_test.c
@@ -12,36 +12,6 @@
 
 #include "../../includes/minishell.h"
 
-void	display_arr(char **arr, char *info)
-{
-	if (!arr)
-		return ;
-	int i = -1;
-	while (arr[++i])
-	{
-		printf("%s", info);
-		printf("[%d] : %s\n", i, arr[i]);
-	}
-}
-
-void	display_cmd(t_list *cmds)
-{
-	int	i,j;
-	
-	j = 0;
-	while (cmds)
-	{
-		i = -1;
-		while (((t_node *)cmds->content)->all_cmd[++i])
-			dprintf(1, "node n.%d cmds[%d] : %s\n", j, i, ((t_node *)cmds->content)->all_cmd[i]);
-		dprintf(1, "node n.%d full_path : %s\n", j, ((t_node *)cmds->content)->all_path);
-		dprintf(1, "node n.%d infile : %d\n", j, ((t_node *)cmds->content)->infile);
-		dprintf(1, "node n.%d outfile : %d\n", j, ((t_node *)cmds->content)->outfile);
-		j++;
-		cmds = cmds->next;
-	}
-}
-
 t_list	*fill_struct_test(t_data *data)
 {
 	t_list	*cmds = NULL;
