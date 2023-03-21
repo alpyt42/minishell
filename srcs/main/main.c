4716bceb5c701b6fd2ffff8253e1120549e2e65c
Alric42Mac <ale-cont@student.42lyon.fr>
2023-03-21 19:01:11 +0100

    done

--- srcs/main/main.c
+++ srcs/main/main.c
@@ -6,7 +6,7 @@
 /*   By: ale-cont <ale-cont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 15:32:53 by ale-cont          #+#    #+#             */
-/*   Updated: 2023/03/21 15:24:00 by ale-cont         ###   ########.fr       */
+/*   Updated: 2023/03/21 19:00:23 by ale-cont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,30 +30,6 @@ void	get_pid(t_data *data)
 	data->p_ids = pid - 1;
 }
 
-static int	special_mini(int argc, t_data *data)
-{
-	char	*prompt;
-
-	if (!argc)
-		return (0);
-	data->exe = 1;
-	signal(SIGINT, signal_receive);
-	signal(SIGQUIT, SIG_IGN);
-	prompt = get_prompt(data);
-	if (argc >= 2 && data->argv && data->argv[1][0] == '-'
-		&& data->argv[1][1] == 'c' && data->argv[2])
-		launch_mini(data, data->argv[2]);
-	else if (argc >= 2)
-	{
-		ft_dprintf(2, ": No such file or directory\n");
-		g_error = 127;
-	}
-	free(prompt);
-	ft_free_mini(data);
-	rl_clear_history();
-	return (g_error);
-}
-
 int	main(int argc, char **argv, char **env)
 {
 	t_data	data;
@@ -62,9 +38,9 @@ int	main(int argc, char **argv, char **env)
 
 	data.env = env;
 	data.argv = argv;
-	init_data(&data);
 	if (argc >= 2 || !argc)
-		return (special_mini(argc, &data));
+		return (0);
+	init_data(&data);
 	while (argc && argv)
 	{
 		data.exe = 1;
